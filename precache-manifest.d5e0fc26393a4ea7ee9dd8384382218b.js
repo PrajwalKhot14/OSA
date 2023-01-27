fc26ed8f87a1c0d546f27169ba6b1ac6659704f7
Prajwal <70838537+PrajwalKhot14@users.noreply.github.com>
2023-01-27 11:55:59 -0600

    Add files via upload

---
+++ precache-manifest.d5e0fc26393a4ea7ee9dd8384382218b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84a4a51b213a03fa897c866cc99662da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6affc4667fd3bf9aeef6",
+    "url": "/static/css/main.a49334e1.chunk.css"
+  },
+  {
+    "revision": "4a3292206123ceb18052",
+    "url": "/static/js/2.823e1ac1.chunk.js"
+  },
+  {
+    "revision": "6ebbaa483fabff7de14b1ae416672e15",
+    "url": "/static/js/2.823e1ac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6affc4667fd3bf9aeef6",
+    "url": "/static/js/main.62254f2e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.62254f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f620b634b443fc7d2dfb",
+    "url": "/static/js/runtime-main.7e6e88a6.js"
+  },
+  {
+    "revision": "4f0a5460deb5addda48194238079d8c9",
+    "url": "/static/media/athlete.4f0a5460.png"
+  }
+]);
